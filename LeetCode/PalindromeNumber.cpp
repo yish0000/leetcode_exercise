1080a0719af1beaa8767723656ce5761b867465b
Yish Shi <yish0000@foxmail.com>
2019-06-19 01:49:34 +0800

    PalindromeNumber

---
+++ LeetCode/PalindromeNumber.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <vector>
+#include "TestCase.h"
+
+class Solution
+{
+public:
+	bool isPalindrome(int x) {
+		if (x < 0) return false;
+		if (x < 10) return true;
+		std::vector<int> arr;
+		while (x > 0)
+		{
+			int part = x % 10;
+			arr.push_back(part);
+			x /= 10;
+		}
+		int s = 0, e = arr.size() - 1;
+		while (s < e)
+		{
+			if (arr[s] != arr[e])
+				return false;
+			s++;
+			e--;
+		}
+		return true;
+	}
+};
+
+RUN_TESTCASE(PalindromeNumber)
+{
+	Solution sln;
+	TESTCASE_ASSERT(sln.isPalindrome(8671768));
+	TESTCASE_ASSERT(sln.isPalindrome(121));
+	TESTCASE_ASSERT(!sln.isPalindrome(-68));
+	TESTCASE_ASSERT(sln.isPalindrome(9));
+	TESTCASE_ASSERT(sln.isPalindrome(3261623));
+	TESTCASE_ASSERT(!sln.isPalindrome(3261663));
+}
