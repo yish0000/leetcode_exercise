ed11e2ad7284e839adff4b11aba6a6035bbc7ca1
Yish <yish0000@gmail.com>
2019-07-10 23:50:22 +0800

    ContainerMostWater

---
+++ LeetCode/ContainerMostWater.cpp
@@ -0,0 +1,27 @@
+#include "TestCase.h"
+
+using namespace std;
+
+class SolutionContainerMostWater {
+public:
+    int maxArea(vector<int>& height) {
+        int result = 0;
+        for (int l=0, r=(int)height.size()-1; l<r;)
+        {
+            result = max(result, min(height[l], height[r]) * (r-l));
+            if (height[l] < height[r])
+                l++;
+            else
+                r--;
+        }
+        
+        return result;
+    }
+};
+
+RUN_TESTCASE(ContainerMostWater)
+{
+    SolutionContainerMostWater sln;
+    vector<int> arr = {1,8,6,2,5,4,8,3,7};
+    TESTCASE_ASSERT(sln.maxArea(arr) == 49);
+}
