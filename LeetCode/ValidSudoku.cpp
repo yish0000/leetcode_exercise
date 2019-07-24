0e2114c1bc893496a93c3fd6acb772f23d2f0870
Yish Shi <yish0000@foxmail.com>
2019-07-24 14:01:21 +0800

    ValidSudoku optimise

--- LeetCode/ValidSudoku.cpp
+++ LeetCode/ValidSudoku.cpp
@@ -6,49 +6,43 @@ class SolutionValidSudoku
 {
 public:
 	bool isValidSudoku(vector<vector<char>>& board) {
-		bool ss[9] = { false };
+		int ss[9] = { false };
 
 		// Horizontal
 		for (size_t i = 0; i < 9; i++)
 		{
 			memset(ss, 0, sizeof(ss));
+
 			for (size_t j = 0; j < 9; j++)
 			{
-				if (board[i][j] == '.') continue;
-				int idx = (board[i][j] - '1');
-				if (ss[idx])
-					return false;
-				ss[idx] = true;
-			}
-		}
+				// Horizontal
+				if (board[i][j] != '.')
+				{
+					int idx = (board[i][j] - '1');
+					if (ss[idx] & 0x1)
+						return false;
+					ss[idx] |= 0x1;
+				}
 
-		// Vertical
-		for (size_t j = 0; j < 9; j++)
-		{
-			memset(ss, 0, sizeof(ss));
-			for (size_t i = 0; i < 9; i++)
-			{
-				if (board[i][j] == '.') continue;
-				int idx = (board[i][j] - '1');
-				if (ss[idx])
-					return false;
-				ss[idx] = true;
-			}
-		}
+				// Vertical
+				if (board[j][i] != '.')
+				{
+					int idx = (board[j][i] - '1');
+					if (ss[idx] & 0x2)
+						return false;
+					ss[idx] |= 0x2;
+				}
 
-		// Nine
-		for (size_t i = 0; i < 9; i++)
-		{
-			memset(ss, 0, sizeof(ss));
-			for (size_t j = 0; j < 9; j++)
-			{
+				// Nine.
 				int x = (i % 3) * 3 + (j % 3);
 				int y = (i / 3) * 3 + (j / 3);
-				if (board[x][y] == '.') continue;
-				int idx = (board[x][y] - '1');
-				if (ss[idx])
-					return false;
-				ss[idx] = true;
+				if (board[x][y] != '.')
+				{
+					int idx = (board[x][y] - '1');
+					if (ss[idx] & 0x4)
+						return false;
+					ss[idx] |= 0x4;
+				}
 			}
 		}
 
