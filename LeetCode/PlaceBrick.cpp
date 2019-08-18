c89af204c110fb58f66016c470af09996a960b42
Yish <yish0000@gmail.com>
2019-08-19 00:38:58 +0800

    Triangle

--- LeetCode/PlaceBrick.cpp
+++ LeetCode/PlaceBrick.cpp
@@ -7,11 +7,24 @@ class SolutionPlaceBrick
 public:
     int placeBrick(int n)
     {
-        return 0;
+        if (n < 3) return 0;
+        vector<int> dp(n, 0);
+        dp[0] = 0;
+        dp[1] = 0;
+        dp[2] = 1;
+        for (int i=3; i<n; i++)
+            dp[i] = dp[i-2] + dp[i-1];
+        return dp[n-1];
     }
 };
 
 RUN_TESTCASE(PlaceBrick)
 {
-    
+    SolutionPlaceBrick sln;
+    TESTCASE_ASSERT(sln.placeBrick(3) == 1);
+    TESTCASE_ASSERT(sln.placeBrick(4) == 1);
+    TESTCASE_ASSERT(sln.placeBrick(5) == 2);
+    TESTCASE_ASSERT(sln.placeBrick(6) == 3);
+    TESTCASE_ASSERT(sln.placeBrick(7) == 4);
+    TESTCASE_ASSERT(sln.placeBrick(8) == 5);
 }
