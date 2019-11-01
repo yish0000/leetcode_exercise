8683990fc56d99101b1e478030b0e51d31286b81
Yish Shi <yish0000@foxmail.com>
2019-11-01 09:19:39 +0800

    RandomPickWithWeight

---
+++ LeetCode/RandomWithBlackList.cpp
@@ -0,0 +1,49 @@
+#include "TestCase.h"
+
+using namespace std;
+
+class SolutionRandomWithBlackList
+{
+public:
+	SolutionRandomWithBlackList(int N, vector<int>& blacklist) {
+		m_N = N - (int)blacklist.size();
+		m_blackList = blacklist;
+		std::sort(m_blackList.begin(), m_blackList.end());
+	}
+
+	int pick() {
+		int n = rand() % m_N;
+		int bnum = (int)m_blackList.size();
+		if (bnum == 0 || m_blackList[0] > n)
+			return n;
+
+		int l = 0;
+		int r = bnum - 1;
+		while (l < r)
+		{
+			int mid = l + (r - l) / 2;
+			if ((n >= m_blackList[mid] - mid) &&
+				(mid == bnum - 1 || (mid != bnum - 1 && n < m_blackList[mid + 1] - mid - 1)))
+				return n + 1 + mid;
+			else if (n < m_blackList[mid] - mid)
+				r = mid - 1;
+			else
+				l = mid + 1;
+		}
+
+		return -1;
+	}
+
+protected:
+	int m_N;
+	vector<int> m_blackList;
+};
+
+RUN_TESTCASE(RandomWithBlackList)
+{
+	/**
+	 * Your Solution object will be instantiated and called as such:
+	 * Solution* obj = new Solution(N, blacklist);
+	 * int param_1 = obj->pick();
+	 */
+}
