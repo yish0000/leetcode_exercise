01573bb62dbdfa5b1a034211a87b62406bd38804
Yish Shi <yish0000@foxmail.com>
2019-11-13 10:51:10 +0800

    NextPermutation SortColors not started

---
+++ LeetCode/SortColors.cpp
@@ -0,0 +1,16 @@
+#include "TestCase.h"
+
+using namespace std;
+
+class SolutionSortColors
+{
+public:
+	void sortColors(vector<int>& nums) {
+
+	}
+};
+
+RUN_TESTCASE(SortColors)
+{
+	TESTCASE_ASSERT(false);
+}
