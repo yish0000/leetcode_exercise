85c7de6585e42c53319af6131681f3a159e92983
Yish Shi <yish0000@foxmail.com>
2019-11-20 19:37:05 +0800

    SortColors

--- LeetCode/SortColors.cpp
+++ LeetCode/SortColors.cpp
@@ -6,11 +6,39 @@ class SolutionSortColors
 {
 public:
 	void sortColors(vector<int>& nums) {
-
+		int l = 0;
+		int r = nums.size() - 1;
+		int mid = 0;
+		while (mid <= r)
+		{
+			if (nums[mid] == 0)
+			{
+				std::swap(nums[mid], nums[l]);
+				mid++;
+				l++;
+			}
+			else if (nums[mid] == 1)
+				mid++;
+			else
+			{
+				std::swap(nums[mid], nums[r]);
+				r--;
+			}
+		}
 	}
 };
 
 RUN_TESTCASE(SortColors)
 {
-	TESTCASE_ASSERT(false);
+	SolutionSortColors sln;
+
+	vector<int> arr1 = { 2,0,2,1,1,0 };
+	vector<int> ret1 = { 0,0,1,1,2,2 };
+	sln.sortColors(arr1);
+	TESTCASE_ASSERT(arr1 == ret1);
+
+	vector<int> arr2 = { 2,0,2,1,1,0,1 };
+	vector<int> ret2 = { 0,0,1,1,1,2,2 };
+	sln.sortColors(arr2);
+	TESTCASE_ASSERT(arr2 == ret2);
 }
