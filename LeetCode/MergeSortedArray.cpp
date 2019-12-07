6efbe5b2532987420d22e44ac27eab3e12aa0ec9
Yish Shi <yish0000@foxmail.com>
2019-12-07 10:00:06 +0800

    MergeSortedArray

---
+++ LeetCode/MergeSortedArray.cpp
@@ -0,0 +1,24 @@
+#include "TestCase.h"
+
+using namespace std;
+
+class SolutionMergeSortedArray
+{
+public:
+	void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+		for (int i = m; i < nums1.size(); i++)
+			nums1[i] = nums2[i - m];
+		std::sort(nums1.begin(), nums1.end());
+	}
+};
+
+RUN_TESTCASE(MergeSortedArray)
+{
+	SolutionMergeSortedArray sln;
+
+	vector<int> arr1 = { 1,2,3,0,0,0 };
+	vector<int> arr2 = { 2,5,6 };
+	vector<int> arr3 = { 1,2,2,3,5,6 };
+	sln.merge(arr1, 3, arr2, 3);
+	TESTCASE_ASSERT(arr1 == arr3);
+}
