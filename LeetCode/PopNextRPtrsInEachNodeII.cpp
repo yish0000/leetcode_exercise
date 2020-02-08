49ea0a78fc6b3f3548ab147fc383230888ec1bbb
yish0000 <yish0000@gmail.com>
2020-02-08 14:48:44 +0800

    PopNextRPtrsInEachNodeII

--- LeetCode/PopNextRPtrsInEachNodeII.cpp
+++ LeetCode/PopNextRPtrsInEachNodeII.cpp
@@ -12,51 +12,44 @@ struct Node {
 	Node(int x) : val(x), left(NULL), right(NULL), next(NULL) {}
 };
 
-class SolutionPopNextRPtrsInEachNode
+class SolutionPopNextRPtrsInEachNodeII
 {
 public:
-	Node* Prev = nullptr;
-
 	Node* connect(Node* root) {
 		if (!root)
 			return root;
-		queue<Node*> q;
-		q.push(root);
-		int n = 0;
+
+		queue<std::pair<Node*, int>> q;
+		q.push(std::make_pair(root, 0));
+		int cur_depth = 0;
+		Node* Prev = nullptr;
+
 		while (q.size() > 0)
 		{
-			Node* node = q.front();
+			std::pair<Node*, int> item = q.front();
 			q.pop();
+
+			Node* node = item.first;
+			int depth = item.second;
 
 			if (Prev)
 				Prev->next = node;
-			n++;
-
-			Prev = ((n + 1) & n) == 0 ? nullptr : node;
 
 			if (node->left)
-				q.push(node->left);
+				q.push(std::make_pair(node->left, depth + 1));
 			if (node->right)
-				q.push(node->right);
+				q.push(std::make_pair(node->right, depth + 1));
+
+			if (q.size() > 0 && q.front().second > depth)
+			{
+				Prev = nullptr;
+				cur_depth = q.front().second;
+			}
+			else
+				Prev = node;
 		}
 		return root;
 	}
-
-	/*
-	void connectHelper(Node* node, int depth)
-	{
-		if (!node)
-			return;
-
-		if (Prev)
-			Prev->next = node;
-
-		Prev = ((n + 1) & n) == 0 ? nullptr : node;
-
-		connectHelper(node->left, ++n);
-		connectHelper(node->right, ++n);
-	}
-	*/
 
 public:
 	Node* MakeTree(const std::vector<std::string>& arr)
@@ -106,18 +99,17 @@ public:
 	}
 };
 
-RUN_TESTCASE(PopNextRPtrsInEachNode)
+RUN_TESTCASE(PopNextRPtrsInEachNodeII)
 {
-	SolutionPopNextRPtrsInEachNode sln;
+	SolutionPopNextRPtrsInEachNodeII sln;
 
-	vector<string> arr1 = { "1","2","3","4","5","6","7" };
+	vector<string> arr1 = { "1","2","3","4","5","null","7" };
 	Node* root1 = sln.MakeTree(arr1);
 	sln.connect(root1);
 	TESTCASE_ASSERT(root1->next == nullptr);
 	TESTCASE_ASSERT(root1->left->next == root1->right && root1->right->next == nullptr);
 	TESTCASE_ASSERT(root1->left->left->next == root1->left->right);
-	TESTCASE_ASSERT(root1->left->right->next == root1->right->left);
-	TESTCASE_ASSERT(root1->right->left->next == root1->right->right);
+	TESTCASE_ASSERT(root1->left->right->next == root1->right->right);
 	TESTCASE_ASSERT(root1->right->right->next == nullptr);
 	sln.FreeTree(root1);
 }
