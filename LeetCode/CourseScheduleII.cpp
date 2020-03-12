c4cc9da109fe1bc5f0416209911be1fa5adcdc3a
yish0000 <yish0000@gmail.com>
2020-03-12 11:45:50 +0800

    CourseScheduleII

--- LeetCode/CourseScheduleII.cpp
+++ LeetCode/CourseScheduleII.cpp
@@ -1,12 +1,13 @@
 #include "TestCase.h"
 #include <queue>
+#include <unordered_set>
 
 using namespace std;
 
 class SolutionCourseScheduleII
 {
 public:
-	vector<int> findOrder(int numCourses, vector<vector<int>>& prerequisites) {
+	vector<int> findOrderBFS(int numCourses, vector<vector<int>>& prerequisites) {
 		vector<vector<int>> g(numCourses);
 		for (auto& preq : prerequisites)
 			g[preq[0]].push_back(preq[1]);
@@ -54,12 +55,48 @@ public:
 		return order;
 	}
 
-	/*
-	void dfs(vector<vector<int>>& g, vector<int>& indegree, int u, vector<int>& order)
-	{
+	vector<int> findOrder(int numCourses, vector<vector<int>>& prerequisites) {
+		vector<vector<int>> g(numCourses);
+		for (auto& preq : prerequisites)
+			g[preq[0]].push_back(preq[1]);
 
+		vector<int> order;
+		vector<bool> visited(numCourses, false);
+		for (int i = 0; i < numCourses; i++)
+		{
+			if (!visited[i])
+			{
+				unordered_set<int> path;
+				if (!dfs(g, i, order, visited, path))
+				{
+					order.clear();
+					return order;
+				}
+			}
+		}
+
+		return order;
 	}
-	*/
+
+	bool dfs(vector<vector<int>>& g, int u, vector<int>& order, vector<bool>& visited, unordered_set<int>& path)
+	{
+		if (visited[u])
+			return true;
+		visited[u] = true;
+
+		path.insert(u);
+		for (auto v : g[u])
+		{
+			if (path.find(v) != path.end())
+				return false;
+
+			if (!dfs(g, v, order, visited, path))
+				return false;
+		}
+		order.push_back(u);
+		path.erase(u);
+		return true;
+	}
 };
 
 RUN_TESTCASE(CourseScheduleII)
@@ -75,6 +112,13 @@ RUN_TESTCASE(CourseScheduleII)
 	vector<vector<int>> arr2 = {
 		{1,0},{2,0},{3,1},{3,2}
 	};
-	vector<int> ret2 = { 0,2,1,3 };
-	TESTCASE_ASSERT(sln.findOrder(4, arr2) == ret2);
+	vector<int> ret2_1 = { 0,2,1,3 };
+	vector<int> ret2_2 = { 0,1,2,3 };
+	TESTCASE_ASSERT(sln.findOrder(4, arr2) == ret2_1 || sln.findOrder(4, arr2) == ret2_2);
+
+	vector<vector<int>> arr3 = {
+		{1,0},{0,1}
+	};
+	vector<int> ret3 = {};
+	TESTCASE_ASSERT(sln.findOrder(2, arr3) == ret3);
 }
