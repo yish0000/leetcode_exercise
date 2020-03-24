444ae634fbed597973b52c63fab39ee9bd8af0ce
yish0000 <yish0000@gmail.com>
2020-03-24 15:37:42 +0800

    LongestValidParentheses

--- LeetCode/LongestValidParentheses.cpp
+++ LeetCode/LongestValidParentheses.cpp
@@ -8,25 +8,6 @@ class SolutionLongestValidParentheses
 public:
 	int longestValidParentheses(string s) {
 		return dp(s);
-		/*
-		stack<int> st;
-		int max_len = 0;
-		st.push(-1);
-		for (int i = 0; i < s.size(); i++)
-		{
-			if (s[i] == '(')
-				st.push(i);
-			else
-			{
-				st.pop();
-				if (st.empty())
-					st.push(i);
-				else
-					max_len = std::max(max_len, i - st.top());
-			}
-		}
-		return max_len;
-		*/
 	}
 
 	int dp(string& s)
@@ -63,6 +44,62 @@ public:
 
 		return max_len;
 	}
+
+	int use_stack(string& s)
+	{
+		stack<int> st;
+		int max_len = 0;
+		st.push(-1);
+		for (int i = 0; i < s.size(); i++)
+		{
+			if (s[i] == '(')
+				st.push(i);
+			else
+			{
+				st.pop();
+				if (st.empty())
+					st.push(i);
+				else
+					max_len = std::max(max_len, i - st.top());
+			}
+		}
+		return max_len;
+	}
+
+	int two_pointer(string& s)
+	{
+		int left = 0, right = 0, maxlength = 0;
+		for (int i = 0; i < s.length(); i++) {
+			if (s[i] == '(') {
+				left++;
+			}
+			else {
+				right++;
+			}
+			if (left == right) {
+				maxlength = std::max(maxlength, 2 * right);
+			}
+			else if (right >= left) {
+				left = right = 0;
+			}
+		}
+		left = right = 0;
+		for (int i = s.length() - 1; i >= 0; i--) {
+			if (s[i] == '(') {
+				left++;
+			}
+			else {
+				right++;
+			}
+			if (left == right) {
+				maxlength = std::max(maxlength, 2 * left);
+			}
+			else if (left >= right) {
+				left = right = 0;
+			}
+		}
+		return maxlength;
+	}
 };
 
 RUN_TESTCASE(LongestValidParentheses)
@@ -77,4 +114,5 @@ RUN_TESTCASE(LongestValidParentheses)
 	TESTCASE_ASSERT(sln.longestValidParentheses("(()(((()(()") == 2);
 	TESTCASE_ASSERT(sln.longestValidParentheses("(()(((()(())") == 6);
 	TESTCASE_ASSERT(sln.longestValidParentheses("())") == 2);
+	TESTCASE_ASSERT(sln.longestValidParentheses(")()()(") == 4);
 }
